fc5003ce49305bae945c4ccdf318b31bd18e8d4e
Diggo0 <168909680+Diggo0@users.noreply.github.com>
2025-02-18 22:19:44 +0000

    opa

--- bd.js
+++ bd.js
@@ -1,7 +1,7 @@
-
-export { selectUsuarios, selectUsuario };
 import pkg from "pg";
+export { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario, updateUsuario };
 const { Pool } = pkg;
+app.use(express.json());
 
 async function connect() {
   const pool = new Pool({
@@ -25,3 +25,34 @@ async function selectUsuario(id) {
     client.release();
     return res.rows;
   }
+
+
+async function selectUsuario(id) {
+  const client = await connect();
+  const query = "SELECT * FROM usuario WHERE id = $1";
+  const usuario = [id];
+  const res = await client.query(query, usuario);
+  client.release();
+  return res.rows;
+}
+async function insertUsuario(data) {
+  const client = await connect();
+  const query = "INSERT INTO usuario (nome,senha,email) VALUES ($1,$2,$3) ";
+  const usuario = [data.nome, data.senha, data.email];
+  await client.query(query, usuario);
+  client.release();
+}
+//bd.js
+async function deleteUsuario(id) {
+  const client = await connect();
+  const query = "DELETE FROM usuario WHERE id = $1";
+  await client.query(query, [id]);
+  client.release();
+}
+async function updateUsuario(id, data) {
+  const client = await connect();
+  const query = "UPDATE usuario SET nome = $1, email = $2, senha = $3 WHERE id = $4";
+  const usuario = [data.nome, data.email, data.senha, id];
+  await client.query(query, usuario);
+  client.release();
+}
