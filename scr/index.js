51d3c026a3299735dc3d0ff247a63eff5a9c701d
Diggo0 <168909680+Diggo0@users.noreply.github.com>
2025-02-20 18:45:07 +0000

    ei

--- scr/index.js
+++ scr/index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
-import roteadorUsuario from "./routes/usuarios";
+import roteadorUsuario from "./routes/usuario.js";
 
 dotenv.config();
 
