30b86531cc5490c6ddd78ac3a7d4323506a3c19c
Diggo0 <168909680+Diggo0@users.noreply.github.com>
2025-02-20 17:18:30 +0000

    finalmente

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
-import { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario, updateUsuario } from "./bd.js";
+import { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario, updateUsuario, } from "./bd.js";
 
 const app = express(); 
 const port = 3000; 
@@ -56,6 +56,21 @@ app.delete("/usuario/:id", async (req, res) => {
       res.status(200).json({ message: "Usuário excluido com sucesso!!" });
     } else res.status(404).json({ message: "Usuário não encontrado!" });
   } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+
+app.put("/usuario/:id", async (req, res) => {
+  console.log("Rota PUT /usuario/# solicitada");
+  try {
+    const id = req.params.id;
+    const usuario = await selectUsuario(id);
+    if (usuario.length > 0) {
+      await updateUsuario(id, req.body);
+      res.status(200).json({ message: "Usuário atualizado com sucesso!" });
+    } else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    console.log(error);
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
 });
