44c408fa0a3bfc9fdf15f89f49c98c5af6033a3c
Diggo0 <168909680+Diggo0@users.noreply.github.com>
2025-02-20 18:36:18 +0000

    ei

--- scr/index.js
+++ scr/index.js
@@ -1,81 +1,24 @@
 import dotenv from "dotenv";
 import express from "express";
-import { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario, updateUsuario, } from "./db/index.js";
+o.jimport roteadorUsuario from "./routes/usuaris";
 
-const app = express(); 
-const port = 3000; 
-
-app.use(express.json());
 dotenv.config();
 
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  console.log("Rota / solicitada");
   res.json({
-    nome: "Gabriel Santos", 
+    message: "API para CRUD usuario: link_gitgub",
   });
 });
 
-app.get("/usuario", async (req, res) => {
-  try {
-    const usuarios = await selectUsuarios();
-    res.json(usuarios);
-  } catch (error) {
-    res.status(error.status || 500).json({ message: error.message || "Erro!" });
-  }
-
-  console.log("Rota GET/usuario solicitada");
-});
-
-
-app.get("/usuario/:id", async (req, res) => {
-  console.log("Rota GET /usuario/# solicitada");
-  try {
-    const usuario = await selectUsuario(req.params.id);
-    if (usuario.length > 0) res.json(usuario);
-    else res.status(404).json({ message: "Usuário não encontrado!" });
-  } catch (error) {
-    res.status(error.status || 500).json({ message: error.message || "Erro!" });
-  }
-});
-
-app.post("/usuario", async (req, res) => {
-  console.log("Rota POST /usuario solicitada");
-  try {
-    await insertUsuario(req.body);
-    res.status(201).json({ message: "Usuário inserido com sucesso!" });
-  } catch (error) {
-    res.status(error.status || 500).json({ message: error.message || "Erro!" });
-  }
-});
-
-app.delete("/usuario/:id", async (req, res) => {
-  console.log("Rota DELETE /usuario/# solicitada");
-  try {
-    const usuario = await selectUsuario(req.params.id);
-    if (usuario.length > 0) {
-      await deleteUsuario(req.params.id);
-      res.status(200).json({ message: "Usuário excluido com sucesso!!" });
-    } else res.status(404).json({ message: "Usuário não encontrado!" });
-  } catch (error) {
-    res.status(error.status || 500).json({ message: error.message || "Erro!" });
-  }
-});
-
-app.put("/usuario/:id", async (req, res) => {
-  console.log("Rota PUT /usuario/# solicitada");
-  try {
-    const id = req.params.id;
-    const usuario = await selectUsuario(id);
-    if (usuario.length > 0) {
-      await updateUsuario(id, req.body);
-      res.status(200).json({ message: "Usuário atualizado com sucesso!" });
-    } else res.status(404).json({ message: "Usuário não encontrado!" });
-  } catch (error) {
-    console.log(error);
-    res.status(error.status || 500).json({ message: error.message || "Erro!" });
-  }
-});
-
 app.listen(port, () => {
+  
   console.log(`Serviço escutando na porta:  ${port}`);
 });
+
+
+app.use(roteadorUsuario);
